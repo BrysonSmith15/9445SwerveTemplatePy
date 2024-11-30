391b4cd622bb19e72bd58a00cf72fcca9444b896
Bryson Smith <15smith.bryson@gmail.com>
2024-11-29 20:45:01 -0600

    Setup pre commit and push hooks with testing and formatting

--- RobotContainer.py
+++ RobotContainer.py
@@ -1,5 +1,7 @@
 from subsystems.swerve_module import SwerveModule
 
+from commands2 import Command
+
 from wpimath.kinematics import SwerveModuleState
 from wpimath.geometry import Rotation2d
 
@@ -7,3 +9,6 @@ from wpimath.geometry import Rotation2d
 class RobotContainer:
     def __init__(self) -> None:
         my_module = SwerveModule("fl", 5, 6, 7)
+
+    def get_auto_command(self) -> Command:
+        return Command()
